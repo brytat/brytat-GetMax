77c97afe7b965e629dc92e5b0cb9fdecdd26f0ef
brytat <brytontateishi@gmail.com>
2023-03-07 02:18:53 +0000

    Get Max Lab

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -8,7 +8,7 @@ public class GetMax {
      */
     public int max(int[] arr){
         int largestNum = arr[0];
-        for (int i = 0; i < arr.length; i++){
+        for (int i = 1; i < arr.length; i++){
             if (arr[i] > largestNum) {
                 largestNum = arr[i];
             }
